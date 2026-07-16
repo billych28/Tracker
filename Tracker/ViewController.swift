3ec56556daf26e6b9eb9d8f22db5eb39c6747f32
Мамытов Руслан <rmamytov@sberbank.ru>
2026-07-16 16:36:08 +0700

    Initial commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Мамытов Руслан on 16.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
